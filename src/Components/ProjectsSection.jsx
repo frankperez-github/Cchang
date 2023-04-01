6c7d1a7aa165c38483838a1d296b3dd12b9d5639
Frank Pérez <fp848584@gmail.com>
2023-04-01 18:38:28 -0400

    review stars

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -1,9 +1,14 @@
+import SiteContext from '@/Context/siteContext.jsx';
+import { useContext } from 'react';
 import ProjectCard from './ProjectCard.jsx'
 function Projects(){
+
+    const { projects } = useContext(SiteContext)
+
     return(
         <div className="Projects mobContainer">
             <h2>Proyectos</h2>
-            <ProjectCard text="Remodelación de las oficinas comerciales de Nissan en Cuba" imagePath="/projectImage.png" dayDate="21" monthDate="03" yearDate="2022" stars="5" reviewsQuant="24"/>
+            <ProjectCard project={projects[0]}/>
             <div className="projectsButtons">
                 <button onClick={()=>(window.open(`/Project?id=1` ,"_self"))} className="siteButton shortButton">Ver Más</button>
                 <button onClick={()=>(window.open("/Projects" ,"_self"))} className="siteButton largeButton">Otros Proyectos</button>
