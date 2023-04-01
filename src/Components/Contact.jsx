84ef4e45fff91ce14df5f5eb503650396f944913
Frank Pérez <fp848584@gmail.com>
2023-04-01 19:24:07 -0400

    font sizes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,9 +10,9 @@ function Contact() {
         <div className="Contact mobContainer">
             <h2>Contacto</h2>
             <div className="networks">
-                <Image onClick={()=>(window.open("https://wa.me/+5350262702"))} className="image networkImage" src="/whatsapp.png" fill />
-                <Image onClick={()=>(window.open("mailto:reynier.baquero@gmail.com"))} className="image networkImage" src="/mail.png" fill />
-                <Image onClick={()=>(window.open("https://www.facebook.com/profile.php?id=100091466821578"))} className="image networkImage" src="/facebook.png" fill />
+                <Image onClick={()=>(window.open("https://wa.me/+5350262702"))} className="image networkImage" src="/whatsapp.png" fill alt=""/>
+                <Image onClick={()=>(window.open("mailto:reynier.baquero@gmail.com"))} className="image networkImage" src="/mail.png" fill alt=""/>
+                <Image onClick={()=>(window.open("https://www.facebook.com/profile.php?id=100091466821578"))} className="image networkImage" src="/facebook.png" fill alt=""/>
             </div>
 
             <textarea id="message" placeholder="Díganos su opinión" name="" cols="42" rows="12" />
