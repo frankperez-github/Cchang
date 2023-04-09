903667ada90884b4a0755f4441e0330143eaa4a3
Frank Pérez <fp848584@gmail.com>
2023-04-09 12:37:29 -0400

    fetching successfully

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getDatabase} from "firebase/database"
+import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,5 +13,5 @@ const firebaseConfig = {
   };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app)
+initializeApp(firebaseConfig);
+export const db = getFirestore()
